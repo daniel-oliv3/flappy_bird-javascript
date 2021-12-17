10512c4fb542dd1f91fe262926a176fe4c24efbb
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-17 14:55:00 -0300

    Update

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+console.log('[Sapup3] Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprintes.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
